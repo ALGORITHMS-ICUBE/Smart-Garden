e5c1d4f20a5376ae4bb55e6e609ca2c721b17ca8
ALGORITHMS-ICUBE <91771099+ALGORITHMS-ICUBE@users.noreply.github.com>
2021-10-02 10:30:37 +0530

    Update SMART_GARDEN.ino

--- SMART_GARDEN.ino
+++ SMART_GARDEN.ino
@@ -1,3 +1,4 @@
+//initializing variables
 int moisture_sensor = A0;
 int motor = A1;
 int relay = 13;
@@ -6,6 +7,7 @@ int moisture_value = 0;
 int motor_value;
 float motor_voltage = 0;
 
+//input and output
 void setup() {
   Serial.begin(9600);
   pinMode(moisture_sensor, INPUT);
@@ -20,20 +22,20 @@ void loop() {
   Serial.print(moisture_value);
   Serial.println();
   delay(100);
-  if (moisture_value >= 500)
+  if (moisture_value >= 500)                           //checks if the soil is dry and needed to be watered 
   {
     digitalWrite(relay, LOW);
     delay(100);
-    motor_value = analogRead(motor);
-    motor_voltage = (5. / 1023.) * motor_value;
+    motor_value = analogRead(motor);                  //only during this condition the motor value is read
+    motor_voltage = (5. / 1023.) * motor_value;       //finding the motor voltage
     Serial.print("   MOTOR VOLTAGE= ");
     Serial.print(motor_voltage);
     Serial.println();
-    if (motor_voltage <= 0.08)
+    if (motor_voltage <= 0.08)                       //motor voltage threshold is set
     {
-      digitalWrite(relay, HIGH);
+      digitalWrite(relay, HIGH);                     //below the voltage threshold the relay is turned OFF
       delay(100);
-      digitalWrite(buzzer, HIGH);
+      digitalWrite(buzzer, HIGH);                    //buzzer ON for indication
       delay(1000);
       digitalWrite(buzzer, LOW);
     }
@@ -43,5 +45,5 @@ void loop() {
     digitalWrite(relay, HIGH);
     delay(100);
   }
-  delay(180000);
+  delay(18000000);                                   //this process should repeat after 5 hours
 }
